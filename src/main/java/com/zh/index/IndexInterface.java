9b5c9f849b7e2da38556d42e64b67ebf2a35102f
zhangheng <1049725878@qq.com>
2021-07-24 00:37:02 +0800

    btree index

---
+++ src/main/java/com/zh/index/IndexInterface.java
@@ -0,0 +1,7 @@
+package com.zh.index;
+
+/**
+ * Created by Administrator on 2021/7/24.
+ */
+public interface IndexInterface {
+}
---
+++ src/main/java/com/zh/index/btree/BtreeIndex.java
@@ -0,0 +1,9 @@
+package com.zh.index.btree;
+
+import com.zh.index.IndexInterface;
+
+/**
+ * Created by Administrator on 2021/7/24.
+ */
+public class BtreeIndex implements IndexInterface {
+}
